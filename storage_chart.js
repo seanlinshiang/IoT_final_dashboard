8c116ce28b9d0d609fb2f397ec6350acce35542b
YehHungYang <zenn19991231@gmail.com>
2023-12-06 17:31:46 +0800

    add storage_chart.js

---
+++ storage_chart.js
@@ -0,0 +1,46 @@
+const storage_ctx = document.getElementById("storage_chart");
+
+const data = {
+  labels: [1, 2, 3, 4, 5],
+  datasets: [
+    {
+      label: "My First Dataset",
+      data: [],
+      backgroundColor: [
+        "rgba(255, 99, 132, 0.2)",
+        "rgba(255, 159, 64, 0.2)",
+        "rgba(255, 205, 86, 0.2)",
+        "rgba(75, 192, 192, 0.2)",
+        "rgba(54, 162, 235, 0.2)",
+        "rgba(153, 102, 255, 0.2)",
+        "rgba(201, 203, 207, 0.2)",
+      ],
+      borderColor: [
+        "rgb(255, 99, 132)",
+        "rgb(255, 159, 64)",
+        "rgb(255, 205, 86)",
+        "rgb(75, 192, 192)",
+        "rgb(54, 162, 235)",
+        "rgb(153, 102, 255)",
+        "rgb(201, 203, 207)",
+      ],
+      borderWidth: 1,
+    },
+  ],
+};
+
+for (let i = 0; i <= 4; i++) {
+  data.datasets[0].data.push(products_data[i].storage);
+}
+
+const storage_chart = new Chart(storage_ctx, {
+  type: "bar",
+  data: data,
+  options: {
+    scales: {
+      y: {
+        beginAtZero: true,
+      },
+    },
+  },
+});
